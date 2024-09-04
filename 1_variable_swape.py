f7c77fee26613852884c3b08f231ad2912694ae5
MD Irfanul Kabir Hira <106048128+irfanulkabirhira@users.noreply.github.com>
2024-09-04 19:11:43 +0600

    Add files via upload

---
+++ 1_variable_swape.py
@@ -0,0 +1,13 @@
+# 1 )Variable Swap: Write a Python program to swap the values of two variables without using a temporary variable.
+
+# Initial values
+a = 5
+b = 10
+
+# Swapping the values
+a, b = b, a
+
+# Output the results
+print("After swapping:")
+print("a =", a)
+print("b =", b)
