31b1ded614d498f894f2747fb6da0c37bc4584f2
MD Irfanul Kabir Hira <106048128+irfanulkabirhira@users.noreply.github.com>
2024-09-05 12:03:57 +0600

    Add files via upload

---
+++ 3_String_Reverse.py
@@ -0,0 +1,9 @@
+# String Reverse: Write a Python function to reverse a given string and return the reversed string.
+
+def reverse_string(s):
+    return s[::-1]
+
+# Example usage
+input_string = "Hello, World!"
+reversed_string = reverse_string(input_string)
+print("Reversed string:", reversed_string)
