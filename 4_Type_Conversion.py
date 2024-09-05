76f34a0a9beb5d6493334520ccf51f8cd5416119
MD Irfanul Kabir Hira <106048128+irfanulkabirhira@users.noreply.github.com>
2024-09-05 12:04:30 +0600

    Add files via upload

---
+++ 4_Type_Conversion.py
@@ -0,0 +1,10 @@
+# Type Conversion: Given a list of integers, write a Python program to convert each element of the list to a string.
+
+# List of integers
+int_list = [1, 2, 3, 4, 5]
+
+# Converting each element to a string
+str_list = [str(i) for i in int_list]
+
+# Output the results
+print("List of strings:", str_list)
